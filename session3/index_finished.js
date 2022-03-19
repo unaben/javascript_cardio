5eaa5df54d5327fc5a0156c2f9fc4de454e8822a
unaben <unaben@yahoo.com>
2022-03-19 11:38:43 +0000

    commit

--- session3/index_finished.js
+++ session3/index_finished.js
@@ -18,7 +18,8 @@ function addAll(...numbers) {
 }
 
 // CHALLENGE 2: SUM ALL PRIMES
-// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// Pass in a number to loop up to and add all of the prime numbers.
+// A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
 function sumAllPrimes(num) {
